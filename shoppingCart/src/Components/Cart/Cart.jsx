9832d04ff969922fb6573e261217bccda7080854
Sunny Kumar <sunny274.kr@gmail.com>
2024-08-31 23:49:40 +0530

    cart update

--- shoppingCart/src/Components/Cart/Cart.jsx
+++ shoppingCart/src/Components/Cart/Cart.jsx
@@ -25,32 +25,34 @@ export default function Cart({ setCartOpen, cart }) {
   return (
     <div className='AddToCart'>
       <div className="wrapper">
+        <span onClick={() => setCartOpen(false)} className="close-btn">x</span>
 
-      <span onClick={() => setCartOpen(false)} className="close-btn">x</span>
+        <div className="cart-header">
+          <h2>Your Cart</h2>
+          <p>Total Items: {cart.length}</p>
+        </div>
 
-      <div className="cart-header">
-        <h2>Your Cart</h2>
-        <p>Total Items: {cart.length}</p>
-      </div>
-
-      {cart.length === 0 ? (
-        <p>Your cart is empty</p>
-      ) : (
-        groupedItemsArray.map((item) => (
-          <div key={item.id} className="cart-item">
-            <div>{item.title}</div>
-            <div className='price-qty'>
-              <div className='price'>Price: ${item.price}</div>
-              <div className='qty'>Quantity: {item.quantity} </div>
-              <div className='total'>Total: ${(item.price * item.quantity).toFixed(2)}</div>
+        {cart.length === 0 ? (
+          <p>Your cart is empty</p>
+        ) : (
+          groupedItemsArray.map((item) => (
+            <div key={item.id} className="cart-item">
+              <div className='img-title'>
+                <img src={item.image} alt="" />
+                <div className='title'>{item.title}</div>
+              </div>
+              <div className='price-qty'>
+                <div className='price'>Price: ${item.price}</div>
+                <div className='qty'>Quantity: {item.quantity} </div>
+                <div className='total'>Total: ${(item.price * item.quantity).toFixed(2)}</div>
+              </div>
             </div>
-          </div>
-        ))
-      )}
-      <div className="cart-footer">
-            <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
-            <button>Proceed to pay</button>
-      </div>
+          ))
+        )}
+        <div className="cart-footer">
+              <h3>Total Bill: ${grandTotal.toFixed(2)}</h3>
+              <button>Proceed to pay</button>
+        </div>
       </div>
     </div>
   );
