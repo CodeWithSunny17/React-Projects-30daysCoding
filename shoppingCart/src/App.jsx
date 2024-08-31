c45624d235511b6aa8fc2d29d925bfa169527a55
Sunny Kumar <sunny274.kr@gmail.com>
2024-08-31 12:52:04 +0530

    day 7

--- shoppingCart/src/App.jsx
+++ shoppingCart/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Shopping from './Components/Shopping/Shopping'
+import Cart from './Components/Cart/Cart'
 
 function App() {
   const [cart, setCart] = useState([]);
